90165b2aa112ec8e1294c6f5204b1214dbe7ce5b
DeMorgaan <60241230+DeMorgaan@users.noreply.github.com>
2021-04-09 16:49:41 +0300

    Updated Player View

--- Views/PlayerV.xaml.cs
+++ Views/PlayerV.xaml.cs
@@ -1,6 +1,10 @@
 ﻿using FlightSimulatorInspection.ViewModels;
+using System;
 using System.Diagnostics;
+using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Threading;
+
 namespace FlightSimulatorInspection.Views
 {
     /// <summary>
@@ -23,47 +27,47 @@ namespace FlightSimulatorInspection.Views
         }
         private void timer_Tick(object sender, EventArgs e)
         {
-            slider_seek.Value = mediaElement.Position.TotalSeconds;
+         //   slider_seek.Value = mediaElement.Position.TotalSeconds;
         }
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
-            mediaElement.Play();
+            //mediaElement.Play();
         }
 
         private void Button_Click_1(object sender, RoutedEventArgs e)
         {
-            mediaElement.Pause();
+            //mediaElement.Pause();
         }
 
         private void Button_Click_2(object sender, RoutedEventArgs e)
         {
-            mediaElement.Stop();
+            //mediaElement.Stop();
         }
 
         private void Slider_seek_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
         {
-            mediaElement.Position = TimeSpan.FromSeconds(slider_seek.Value);
+            //mediaElement.Position = TimeSpan.FromSeconds(slider_seek.Value);
         }
 
-        private void MediaElement_MediaOpened(object sender, RoutedEventArgs e)
-        {
-            TimeSpan ts = mediaElement.NaturalDuration.TimeSpan;
-            slider_seek.Maximum = ts.TotalSeconds;
-            timer.Start();
-        }
+     //   private void MediaElement_MediaOpened(object sender, RoutedEventArgs e)
+     //   {
+     //       TimeSpan ts = //mediaElement.NaturalDuration.TimeSpan;
+     //       slider_seek.Maximum = ts.TotalSeconds;
+     //       timer.Start();
+     //   }
 
         private void Window_Drop(object sender, DragEventArgs e)
         {
             string filename = (string)((DataObject)e.Data).GetFileDropList()[0];
-            mediaElement.Source = new Uri(filename);
-            mediaElement.LoadedBehavior = MediaState.Manual;
-            mediaElement.UnloadedBehavior = MediaState.Manual;
-            mediaElement.Play();
+            //mediaElement.Source = new Uri(filename);
+            //mediaElement.LoadedBehavior = MediaState.Manual;
+            //mediaElement.UnloadedBehavior = MediaState.Manual;
+            //mediaElement.Play();
         }
         private void Slider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
         {
-            mediaElement.SpeedRatio = (double)speedRatioSlider.Value;
+            //mediaElement.SpeedRatio = (double)speedRatioSlider.Value;
         }
     }
 }
