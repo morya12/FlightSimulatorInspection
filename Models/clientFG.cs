cc89547794942c5f622393bf1e0285aee61b25d1
SHAI GUNDERSEN <shai.gundersen@gmail.com>
2021-04-13 15:39:15 +0300

    udated ClientFG

--- Models/clientFG.cs
+++ Models/clientFG.cs
@@ -1,74 +1,12 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.IO;
 using System.Net;
 using System.Net.Sockets;
-using System.Threading;
 
 namespace FlightSimulatorInspection.Models
 {
     class ClientFG
     {
-        private string csvFilePath;
-        private string xmlFilePath;
-        private int speed;
-        public string XmlFilePath
-        {
-            get
-            {
-                return this.xmlFilePath;
-            }
-            set
-            {
-                this.xmlFilePath = value;
-            }
-        }
-        public string CsvFilePath
-        {
-            get
-            {
-                return this.csvFilePath;
-            }
-            set
-            {
-                this.csvFilePath = value;
-            }
-        }
-        public int Speed
-        {
-            get
-            {
-                if (speed == 0)
-                {
-                    return 1;
-                }
-                return speed;
-            }
-            set
-            {
-                if (value < 0)   //invalid speed. do not update
-                {
-                    return;
-                }
-                this.speed = value;
-            }
-        }
         public int host = 5400;
-
-        public ClientFG(string xmlFile, string csvFile)
-        {
-            this.xmlFilePath = xmlFile;
-            this.csvFilePath = csvFile;
-        }
-
-        public ClientFG()
-        {
-
-        }
-
         public Socket connect()
         {
             Socket s = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
